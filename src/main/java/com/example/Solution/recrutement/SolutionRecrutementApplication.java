8184099bb0e4b434e23dc8dfce289d87e1655587
bensalah77 <ahmed.bensalah@esprit.tn>
2023-05-06 12:44:50 +0100

    archi

---
+++ src/main/java/com/example/Solution/recrutement/SolutionRecrutementApplication.java
@@ -0,0 +1,13 @@
+package com.example.Solution.recrutement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutionRecrutementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolutionRecrutementApplication.class, args);
+	}
+
+}
